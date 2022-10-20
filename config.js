e08f313aae3600bc4234bee6221bab0e60dc1916
Veagle234 <115721730+Veagle234@users.noreply.github.com>
2022-10-20 13:44:22 +0700

    me

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Mẹ yêu';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Gửi Mẹ yêu';
+const contentCard = 'Nhân ngày phụ nữa Việt Nam 20/10, con chúc Mẹ, người phụ nữ con yêu nhất trên cuộc đời này luôn xinh đẹp, mạnh khỏe và hạnh phúc. Cảm ơn mẹ đã luôn yêu thương, che chở cho con suốt những năm tháng qua. Con yêu mẹ nhiều lắm!';
+
+
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
